ab2c3de5d7d661ea327a61a008969620275d8b32
Volodymyr <bovsisi5@gmail.com>
2024-12-24 00:05:01 +0100

    Initial commit

---
+++ js/file.js
@@ -0,0 +1,56 @@
+// Получаем элементы
+const accountBtn = document.getElementById('accountBtn');
+const loginModal = document.getElementById('loginModal');
+const logincart = document.getElementById('logincart');
+const closeBtn1 = document.querySelector('.close-btn1');
+const closeBtn2 = document.querySelector('.close-btn2');
+const cartBtn = document.getElementById("cartBtn");
+
+// Функция для включения/выключения прокрутки страницы
+function toggleBodyScroll(isBlocked) {
+    if (isBlocked) {
+        document.body.style.overflow = 'hidden';  // Блокирует прокрутку
+    } else {
+        document.body.style.overflow = '';  // Включает прокрутку
+    }
+}
+
+// Открыть модальное окно для авторизации
+accountBtn.addEventListener('click', () => {
+    loginModal.style.display = 'block';
+    toggleBodyScroll(true);  // Блокируем прокрутку
+});
+
+// Закрыть модальное окно при клике на кнопку закрытия
+closeBtn1.addEventListener('click', () => {
+    loginModal.style.display = 'none';
+    toggleBodyScroll(false);  // Включаем прокрутку
+});
+
+// Закрыть модальное окно при клике вне его
+window.addEventListener('click', (event) => {
+    if (event.target === loginModal) {
+        loginModal.style.display = 'none';
+        toggleBodyScroll(false);  // Включаем прокрутку
+    }
+});
+
+// Открыть окно с корзиной
+cartBtn.addEventListener('click', () => {
+    logincart.style.display = 'block';
+    toggleBodyScroll(true);  // Блокируем прокрутку
+});
+
+// Закрыть окно с корзиной при клике на кнопку закрытия
+closeBtn2.addEventListener('click', () => {
+    logincart.style.display = 'none';
+    toggleBodyScroll(false);  // Включаем прокрутку
+});
+
+// Закрыть окно с корзиной при клике вне его
+window.addEventListener('click', (event) => {
+    if (event.target === logincart) {
+        logincart.style.display = 'none';
+        toggleBodyScroll(false);  // Включаем прокрутку
+    }
+});
